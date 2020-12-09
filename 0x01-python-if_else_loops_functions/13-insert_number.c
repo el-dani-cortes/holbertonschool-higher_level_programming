d86680aeae2ace1fc5dda1dafd3d1b76267e22cd
el-dani-cortes <2304@holbertonschool.com>
2020-12-09 03:17:00 +0000

    Changed the first if condition to evaluate head pointer exist

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -13,7 +13,7 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *tmp, *advance, *new_node;
 
-	if ((*head) == NULL)
+	if (head == NULL)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
