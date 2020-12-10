5e90544b68612a072cd102703efce7fab47e0cba
el-dani-cortes <2304@holbertonschool.com>
2020-12-10 20:04:59 +0000

    Deleted a for at the beginning of the code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    for arguments in argv:
-        if len(argv) < 2:
-            print("0 arguments.")
-        elif len(argv) == 2:
-            print("1 argument:")
-            print("1:", argv[1])
-            break
-        elif len(argv) > 2:
-            print("{0:d} arguments:".format(len(argv) - 1))
-            n = 1
-            for i in argv[1:]:
-                print("{0:d}: {1:s}".format(n, i))
-                n = n + 1
-                break
+    if len(argv) < 2:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("1:", argv[1])
+    elif len(argv) > 2:
+        print("{0:d} arguments:".format(len(argv) - 1))
+        n = 1
+        for i in argv[1:]:
+            print("{0:d}: {1:}".format(n, i))
+            n = n + 1
