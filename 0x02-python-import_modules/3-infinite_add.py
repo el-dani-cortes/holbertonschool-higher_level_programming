f52de7537718ffa175dc7e4a6fc5b3e6f45f840c
el-dani-cortes <2304@holbertonschool.com>
2020-12-10 21:22:17 +0000

    Added a equal condition in the last condition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,8 +3,8 @@ if __name__ == "__main__":
     from sys import argv
     if len(argv) < 2:
         print("0")
-    elif len(argv) > 2:
+    elif len(argv) >= 2:
         sum = 0
         for i in argv[1:]:
             sum = sum + int(i)
-            print(sum)
+        print(sum)
