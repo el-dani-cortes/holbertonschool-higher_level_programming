b9b17578f4b77dea6c619677bf694e190353629c
el-dani-cortes <2304@holbertonschool.com>
2020-12-10 17:59:05 +0000

    Changed the declaration for import module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     print("{0:d} + {1:d} = {2:d}".format(a, b, calculator_1.add(a, b)))
