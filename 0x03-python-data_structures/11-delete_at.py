b19c42463c3f2a9822553b77174340fccdec8e9a
el-dani-cortes <2304@holbertonschool.com>
2020-12-15 19:44:00 +0000

    Deleted equal in if condition to compare with lenght of the list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -9,13 +9,12 @@ def delete_at(my_list=[], idx=0):
     Returns:
         Return a the list without the item removed.
     """
-    if my_list:
-        n = 0
-        if idx >= 0 and idx <= len(my_list):
-            for item in my_list:
-                if n == idx:
-                    del my_list[idx]
-                n = n + 1
-            return my_list
-        else:
-            return my_list
+    n = 0
+    if idx >= 0 and idx < len(my_list):
+        for item in my_list:
+            if n == idx:
+                del my_list[idx]
+            n = n + 1
+        return my_list
+    else:
+        return my_list
