8303750a8623d8d8878aad32fae72d81e71f99c9
el-dani-cortes <2304@holbertonschool.com>
2020-12-15 17:20:33 +0000

    Added a function that adds 2 tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    """Function that adds 2 tuples
+
+    Args:
+        tuple_a: data type tuple a
+        tuple_b: data type tuple b
+
+    Returns:
+        Return the sum of tuples with two integers
+    """
+    tuple_sum = (0, 0)
+    if len(tuple_b) < 2 and len(tuple_b) > 0:
+        tuple_b = (tuple_b[0], 0)
+    elif len(tuple_a) < 2 and len(tuple_a) > 0:
+        tuple_a = (tuple_a[0], 0)
+    elif len(tuple_a) == 0:
+        tuple_a = (0, 0)
+    elif len(tuple_b) == 0:
+        tuple_b = (0, 0)
+    tuple_sum = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    return tuple_sum
