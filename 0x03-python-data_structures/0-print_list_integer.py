64a49bb62e8fbca3ffbc8b394141ec6fca9b8098
el-dani-cortes <2304@holbertonschool.com>
2020-12-15 17:06:48 +0000

    Added function that prints all integers of a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """Function that prints a list
+
+    Args:
+        my_list: list to print
+
+    Returns:
+        Always nothing.
+    """
+    for number in my_list:
+        print("{0:d}".format(number))
