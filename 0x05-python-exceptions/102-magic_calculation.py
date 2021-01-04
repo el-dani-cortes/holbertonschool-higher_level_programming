293e7a1decb61a0c4bba072d9f1cc5ef563aeaf2
el-dani-cortes <danicor83@gmail.com>
2021-01-04 13:26:32 -0500

    Put uppercase the first letter of exception to print

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -6,10 +6,10 @@ def magic_calculation(a, b):
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception('too far')
+                raise Exception('Too far')
             else:
                 result += (a ** b) / i
         except:
             result = a + b
             break
-        return result
+    return result
