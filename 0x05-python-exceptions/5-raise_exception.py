e2fa600ae17cf81c8b4bc42133b9cc2a289631d9
el-dani-cortes <danicor83@gmail.com>
2021-01-04 12:12:21 -0500

    Added a function that raises a type exception

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def raise_exception():
+    """Function that raises a type exception.
+    Args:
+        Any argument
+    Returns:
+        Always nothing.
+    """
+    raise TypeError()
