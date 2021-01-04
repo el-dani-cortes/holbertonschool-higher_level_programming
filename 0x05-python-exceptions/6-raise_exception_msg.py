959a529b103926c4b4c3a87881f2cae6f84dc2d9
el-dani-cortes <danicor83@gmail.com>
2021-01-04 12:12:50 -0500

    Added a function that raises a name exception with a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """Function that raises a name exception with a message.
+    Args:
+        Any argument
+    Returns:
+        Always nothing.
+    """
+    raise NameError(message)
