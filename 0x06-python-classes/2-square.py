348c78048414aadc95062c881e53e72abda98b24
el-dani-cortes <danicor83@gmail.com>
2021-01-05 12:26:04 -0500

    Added a class Square that defines a square (based on 1-square.py)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Class that defines a square"""
+    def __init__(self, size=0):
+        """Method for initialize a square object
+           with validation of the data type of size variable.
+
+        Args:
+            size(int): Size of the side of the square.
+
+        """
+        if isinstance(size, int) is True:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be an integer')
