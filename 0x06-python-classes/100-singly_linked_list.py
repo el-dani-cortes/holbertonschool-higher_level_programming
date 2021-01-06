294bfee0dead26f32112fd9ce195a3f0a68d67b2
el-dani-cortes <danicor83@gmail.com>
2021-01-06 15:53:51 -0500

    Added a class for singly linked list and a class for Node

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -72,10 +72,10 @@ class Node():
            Always nothing.
 
         """
-        if type(value) is not Node or value is not None:
+        if type(value) is not Node and value is not None:
             raise TypeError('next_node must be a Node object')
-        else:
-            self.__next_node = value
+        self.__next_node = value
+
 
 class SinglyLinkedList():
     """Class that defines a singly linked list"""
@@ -103,19 +103,17 @@ class SinglyLinkedList():
 
         """
         new_node = Node(value, None)
+        tmp = self.__head
         if self.__head is None:
             self.__head = new_node
-        elif value < self.__head.data:
+        elif value <= self.__head.data:
             new_node.next_node = self.__head
             self.__head = new_node
         else:
-            tmp = self.__head
-            while tmp.next_node is not None:
-                if value > tmp.next_node.data:
-                    new_node.next_node = tmp.next_node
-                    tmp.next_node = new_node
-                    break
+            while tmp.next_node and value > tmp.next_node.data:
                 tmp = tmp.next_node
+            new_node.next_node = tmp.next_node
+            tmp.next_node = new_node
 
     def __str__(self):
         """Method to print Square instance.
@@ -125,10 +123,11 @@ class SinglyLinkedList():
 
         """
         node_data = ""
-        if self.__head == None:
+        if self.__head is None:
             return ""
         else:
             tmp = self.__head
-            while tmp.next_node is not None:
+            while tmp is not None:
                 node_data += str(tmp.data) + "\n"
-        return node_data
+                tmp = tmp.next_node
+        return node_data[:-1]
