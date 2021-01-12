fd395121c6ffd3b83c4e2fb7c61e1a86ae42f2f1
el-dani-cortes <danicor83@gmail.com>
2021-01-11 20:10:54 -0500

    Changed order call of setter and getter of width and height

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -8,41 +8,19 @@ class Rectangle:
     print_symbol = '#'
 
     def __init__(self, width=0, height=0):
-        """Init method to initialized a instance"""
-        self.height = height
+        """Init method to initialized a instance.
+
+        Args:
+           width(int): Size of the width of the rectangle.
+           height(int): Size of the height of the rectangle.
+
+        Return:
+            The current value of the width atributte.
+
+        """
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
-
-    @property
-    def width(self):
-        """Method getter for width atributte
-
-        Args:
-            Not arguments
-
-        Return:
-            The current value of the width atributte
-
-        """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Method setter for width atributte
-
-        Args:
-            value(int): width value of the rectangle
-
-        Return:
-            Always nothing
-
-        """
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
 
     @property
     def height(self):
@@ -75,6 +53,37 @@ class Rectangle:
         else:
             self.__height = value
 
+    @property
+    def width(self):
+        """Method getter for width atributte
+
+        Args:
+            Not arguments
+
+        Return:
+            The current value of the width atributte
+
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Method setter for width atributte
+
+        Args:
+            value(int): width value of the rectangle
+
+        Return:
+            Always nothing
+
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
     def area(self):
         """Method that calculates area of a rectangle.
 
@@ -85,8 +94,7 @@ class Rectangle:
             Area value of the rectangle.
 
         """
-        Rectangle.number_of_instances += 1
-        return self.__width * self.__height
+        return self.width * self.height
 
     def perimeter(self):
         """Method that calculates perimeter of a rectangle.
@@ -98,11 +106,10 @@ class Rectangle:
             Perimeter value of the rectangle.
 
         """
-        Rectangle.number_of_instances += 1
-        if self.__width == 0 or self.__height == 0:
+        if self.width == 0 or self.height == 0:
             return 0
         else:
-            return (self.__width + self.__height) * 2
+            return (self.width + self.height) * 2
 
     def __str__(self):
         """Method that returns a rectangle by # character.
@@ -114,9 +121,11 @@ class Rectangle:
             A rectangle represented by character #.
 
         """
-        hash_line = str(self.print_symbol) * self.__width
-        rectangle = (hash_line + '\n') * (self.__height - 1) + hash_line
-        Rectangle.number_of_instances += 1
+        if self.width == 0 or self.height == 0:
+            return ""
+        else:
+            hash_line = str(self.print_symbol) * self.width
+            rectangle = (hash_line + '\n') * (self.height - 1) + hash_line
         return rectangle
 
     def __repr__(self):
@@ -130,9 +139,8 @@ class Rectangle:
             Formal representation of a object instance.
 
         """
-        Rectangle.number_of_instances += 1
         representation = "Rectangle({}, {})".format(
-            self.__width, self.__height)
+            self.width, self.height)
         return representation
 
     def __del__(self):
