43b5eb6dc64c3dcc15a7a58aef679df3899b51be
el-dani-cortes <danicor83@gmail.com>
2021-01-19 18:58:00 -0500

    Changed the order of the init validations and put a string literal of width and height

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -18,9 +18,9 @@ class Rectangle(BaseGeometry):
                    Always nothing.
 
                 """
-                self.integer_validator(str(width), width)
+                self.integer_validator("width", width)
+                self.integer_validator("height", height)
                 self.__width = width
-                self.integer_validator(str(height), height)
                 self.__height = height
 
         def area(self):
