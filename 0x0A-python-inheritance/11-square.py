fb3dc9e4ebb536fb9326e5947d6d3bd493528ce8
el-dani-cortes <danicor83@gmail.com>
2021-01-19 17:43:22 -0500

    Added a class Square that inherits from Rectangle (9-rectangle.py), with a str method that returns: [Square] <width>/<height>

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+"""Module that defines a class geometry"""
+
+
+class BaseGeometry:
+        """Class that defines geometry"""
+
+        def area(self):
+                """Method that raises an Exception"""
+                raise Exception("area() is not implemented")
+
+        def integer_validator(self, name, value):
+                """Method that validates a value
+
+                Args:
+                   name(string): name of the value.
+                   value(int): data to validate.
+
+                Return:
+                   Always nothing.
+
+                """
+                if type(value) is not int:
+                        raise TypeError("{} must be an integer".format(name))
+                if value <= 0:
+                        raise ValueError("{} must be greater than 0".
+                                         format(name))
+
+
+class Rectangle(BaseGeometry):
+        """Define a class Rectangle that inherits from BaseGeometry"""
+
+        def __init__(self, width, height):
+                """Method that init a rectangle object.
+
+                Args:
+                   width(int): Size of the rectangle's width.
+                   height(int): Size of the rectangle's height.
+
+                Return:
+                   Always nothing.
+
+                """
+                self.integer_validator(str(width), width)
+                self.__width = width
+                self.integer_validator(str(height), height)
+                self.__height = height
+
+        def area(self):
+                """Method that returns the area of a rectangle"""
+                return (self.__width * self.__height)
+
+        def __str__(self):
+                """Method that returns a specific string.
+
+                Args:
+                   Not arguments.
+
+                Return:
+                   A specific string: [Rectangle] <width>/<height>.
+
+                """
+                return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+        """Defines a Square class"""
+
+        def __init__(self, size):
+                """Method that init a square object.
+
+                Args:
+                   size(int): Size of the square's width.
+
+                Return:
+                   Always nothing.
+
+                """
+                self.integer_validator(str(size), size)
+                self.__size = size
+                super().__init__(size, size)
+
+        def area(self):
+                """Method that returns the area of a square"""
+                return super().area()
+
+        def __str__(self):
+                """Method that returns a specific string.
+
+                Args:
+                   Not arguments.
+
+                Return:
+                   A specific string: [Square] <width>/<height>.
+
+                """
+
+                return "[Square] {}/{}".format(self.__size, self.__size)
