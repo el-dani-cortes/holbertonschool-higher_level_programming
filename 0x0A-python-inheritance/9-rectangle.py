200049d881fa3f5a63ec0c089896e46d9ea169df
el-dani-cortes <danicor83@gmail.com>
2021-01-19 18:55:16 -0500

    Added a import of the class BaseGeometry from the module 7-base_geometry

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,30 +1,7 @@
 #!/usr/bin/python3
-"""Module that defines a class geometry"""
+"""Module that defines a class Rectangle"""
 
-
-class BaseGeometry:
-        """Class that defines geometry"""
-
-        def area(self):
-                """Method that raises an Exception"""
-                raise Exception("area() is not implemented")
-
-        def integer_validator(self, name, value):
-                """Method that validates a value
-
-                Args:
-                   name(string): name of the value.
-                   value(int): data to validate.
-
-                Return:
-                   Always nothing.
-
-                """
-                if type(value) is not int:
-                        raise TypeError("{} must be an integer".format(name))
-                if value <= 0:
-                        raise ValueError("{} must be greater than 0".
-                                         format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
