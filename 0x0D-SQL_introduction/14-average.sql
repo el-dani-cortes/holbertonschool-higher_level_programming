beb13d0a02599a1b85a1f2dcb36c49928a8c4c48
el-dani-cortes <danicor83@gmail.com>
2021-03-01 18:11:03 +0000

    Added a AS statement to print the output exactly holberton ask

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- Computes the score average of all records in the table second_table
 -- of the database hbtn_0c_0 in MySQL server.
 -- The result column name should be average.
-SELECT AVG(ALL score) FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
