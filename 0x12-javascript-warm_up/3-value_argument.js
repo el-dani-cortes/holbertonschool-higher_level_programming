1a992e3ca0d27cd5e1101f465566df3eb78fd1cd
el-dani-cortes <danicor83@gmail.com>
2021-03-22 17:22:25 +0000

    Chenged the condition to validate the numbers of arguments pass to the program

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /*  script that prints the first argument passed to it */
 const args = process.argv;
-if (args.length <= 2) {
+if (args.length !== 3) {
   console.log('No argument');
 } else {
   console.log(args[2]);
