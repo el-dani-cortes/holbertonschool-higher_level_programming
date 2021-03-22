25517c0dfffb51936b0e380e99200772de55b900
el-dani-cortes <danicor83@gmail.com>
2021-03-22 17:27:30 +0000

    Changed the way validate the arguments

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-/*  script that prints the first argument passed to it */
+/*  Script that prints the first argument passed to it */
 const args = process.argv;
-if (args.length !== 3) {
-  console.log('No argument');
-} else {
+if (args[2]) {
   console.log(args[2]);
+} else {
+  console.log('No argument');
 }
