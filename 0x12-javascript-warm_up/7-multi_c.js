9cddb586ad73589ba2b94943536d9fe19a5dbb6a
el-dani-cortes <danicor83@gmail.com>
2021-03-22 17:47:38 +0000

    Removed one console.log. The task ask for two only

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
 /* Script that prints x times "C is fun" */
 const args = process.argv;
-if (args.length !== 3) {
+if (args.length !== 3 || isNaN(parseInt(args[2]))) {
   console.log('Missing number of occurrences');
 } else {
   let num = parseInt(args[2]);
-  if (isNaN(num)) {
-    console.log('Missing number of occurrences');
-  } else {
-    while (num > 0) {
-      console.log('C is fun');
-      num--;
-    }
+  while (num > 0) {
+    console.log('C is fun');
+    num--;
   }
 }
