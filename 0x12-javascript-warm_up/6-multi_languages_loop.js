3d36bf643e7bb6c7bc6c5063547d71bb1218bf08
el-dani-cortes <danicor83@gmail.com>
2021-03-22 17:38:48 +0000

    Changed name variable

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,9 +3,9 @@
 * Script that prints 3 lines: (like 1-multi_languages.js)
 * but by using an array of string and a loop.
 */
-const myvar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
-while (i < myvar.length) {
-  console.log(myvar[i]);
+while (i < array.length) {
+  console.log(array[i]);
   i++;
 }
