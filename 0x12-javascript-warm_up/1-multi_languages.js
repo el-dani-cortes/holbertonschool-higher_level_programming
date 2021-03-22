58b6ac0faaede59eb87171bcf12fc548c5d93835
el-dani-cortes <danicor83@gmail.com>
2021-03-22 17:18:04 +0000

    Put all the output in an one console.log

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
 /* Script that prints 3 lines */
-let myvar = 'C is fun';
-console.log(myvar);
-myvar = 'Python is cool';
-console.log(myvar);
-myvar = 'JavaScript is amazing';
-console.log(myvar);
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
