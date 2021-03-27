3f724503366f1de639307a60392de01e106ed7df
el-dani-cortes <danicor83@gmail.com>
2021-03-27 11:23:23 +0000

    Changed the comments and changed the number of characters in the name attribute

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Class definition of a State and an instance Base = declarative_base()
+Class definition of a State and an
+instance Base = declarative_base().
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -12,4 +13,4 @@ Base = declarative_base()
 class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
-    name = Column(String(180), nullable=False)
+    name = Column(String(128), nullable=False)
