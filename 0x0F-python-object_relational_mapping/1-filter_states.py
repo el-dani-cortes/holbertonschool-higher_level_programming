b5bd4e4678e1eeeba4a86eabaa4561db93439f00
el-dani-cortes <danicor83@gmail.com>
2021-03-27 10:57:59 +0000

    Added a a order sql command according to the task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
     d_base = sys.argv
     conn = mdb.connect(**args)
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'\
+    ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
