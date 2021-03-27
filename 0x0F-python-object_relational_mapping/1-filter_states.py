86ba351d1617850d2ea47d6f2f7ef337ff71b832
el-dani-cortes <danicor83@gmail.com>
2021-03-27 14:43:16 +0000

    Added another condition to filter according to the task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -22,6 +22,7 @@ if __name__ == "__main__":
     ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1][0] == 'N':
+            print(row)
     cur.close()
     conn.close()
