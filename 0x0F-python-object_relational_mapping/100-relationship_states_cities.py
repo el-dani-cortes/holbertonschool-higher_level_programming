a3151d1f7c62718707167b18dbe39deccfdabdbe
el-dani-cortes <danicor83@gmail.com>
2021-03-27 11:48:15 +0000

    Changed the way of use the relationship method from ORM

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -3,10 +3,10 @@
 Script that prints all City objects from the database hbtn_0e_14_usa.
 """
 import sys
-from model_state import Base, State
-from model_city import Base, City
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import (create_engine)
-from sqlalchemy.orm import Session, relationship
 
 
 if __name__ == "__main__":
@@ -14,10 +14,9 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    State.city = relationship("City")
-    s1 = State(name="California")
-    s1.city = [City(name="San Francisco")]
-    session = Session(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    s1 = State(name='California', cities=[City(name='San Francisco')])
     session.add(s1)
     session.commit()
     session.close()
