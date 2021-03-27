35a1659797ed5f998de059463fb068613d720434
el-dani-cortes <danicor83@gmail.com>
2021-03-27 11:46:56 +0000

    Added comments to the class definition

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """
-Class definition of a City
+This module defines a City class that
+inherits from Base class from module SQLAlchemy.
 """
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base, State
+from sqlalchemy import ForeignKey, Column, Integer, String
+from relationship_state import Base
 
 
 class City(Base):
+    """
+    Represents an instance of City class for SQLAlchemy module
+    """
     __tablename__ = 'cities'
-    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
-    name = Column(String(180), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """
-Class definition of a State and an instance Base = declarative_base()
+This module defines a State class that inherits
+from Base class from module SQLAlchemy.
 """
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-
-
-
 Base = declarative_base()
 
 
 class State(Base):
+    """
+    Represents an instance of State class for SQLAlchemy module.
+    """
     __tablename__ = 'states'
-    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
-    name = Column(String(180), nullable=False)
-    state = relationship("City")
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete')
