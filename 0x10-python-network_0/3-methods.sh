144eeb74476564a412c2cf602bce0ec7b2ad13e9
el-dani-cortes <danicor83@gmail.com>
2021-04-08 19:32:30 +0000

    UPDATED | Put  to receive the ip from the command line terminal

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL and displays all HTTP methods the server will accept
-curl -sI 0.0.0.0:5000/route_4 | grep -i "Allow" | awk -F ": " '{ print $2 }'
+curl -sI "$1" | grep -i "Allow" | awk -F ": " '{ print $2 }'
