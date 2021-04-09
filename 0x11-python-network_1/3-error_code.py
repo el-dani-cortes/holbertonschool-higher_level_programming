dfac6922c43dcd280b54e6a7407e200f78bae9f5
el-dani-cortes <danicor83@gmail.com>
2021-04-09 18:40:32 +0000

    UPDATED | Changed the print of the error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -18,7 +18,7 @@ def main(argv):
             result = response.read()
             print(result.decode('utf8'))
     except urllib.error.URLError as e:
-        print("Error code:{}".format(e.code))
+        print("Error code: {}".format(e.code))
 
 
 if __name__ == "__main__":
